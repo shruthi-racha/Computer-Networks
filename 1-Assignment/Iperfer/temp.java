ae9fae63889f3e17c4ffc272e1a1ed0ba93ef9a6
Adithya Bhat <adithya.bhatp2@gmail.com>
2016-02-18 17:39:12 -0600

    setup for assign 2

---
+++ 1-Assignment/Iperfer/temp.java
@@ -0,0 +1,17 @@
+
+public class temp
+	{
+
+	public static void main(String[] args)
+		{
+		// TODO Auto-generated method stub
+		byte[] oneKilo = new byte[1000];
+		
+		for(int i=0;i<oneKilo.length;i++)
+			{
+			System.out.println(oneKilo[i]);
+			}
+		
+		}
+
+	}
