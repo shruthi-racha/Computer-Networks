e60a26f8968cb46533956624a5581a4609e7ad44
adbhat <adithya.bhatp2@gmail.com>
2016-02-20 16:26:44 -0600

    adding assign_2 java project

---
+++ 2_Assignment_java/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -0,0 +1,101 @@
+package edu.wisc.cs.sdn.vnet.sw;
+
+import net.floodlightcontroller.packet.Ethernet;
+import edu.wisc.cs.sdn.vnet.Device;
+import edu.wisc.cs.sdn.vnet.DumpFile;
+import edu.wisc.cs.sdn.vnet.Iface;
+import java.time.LocalTime;
+import java.util.*;
+import net.floodlightcontroller.packet.MACAddress;
+
+/**
+ * @author Aaron Gember-Jacobson
+ */
+
+class SwitchTableEntry
+	{
+	Iface outIface;
+	LocalTime inputTime;
+	
+	public SwitchTableEntry(Iface iface)
+	{
+	this.outIface = iface;
+	this.inputTime = LocalTime.now();
+	}
+	
+	}
+
+public class Switch extends Device
+	{
+	/**
+	 * Creates a router for a specific host.
+	 * 
+	 * @param host
+	 *            hostname for the router
+	 */
+
+	HashMap<MACAddress, SwitchTableEntry> macIfLookup;
+
+	public Switch(String host, DumpFile logfile)
+		{
+		super(host, logfile);
+		}
+
+	/**
+	 * Handle an Ethernet packet received on a specific interface.
+	 * 
+	 * @param etherPacket
+	 *            the Ethernet packet that was received
+	 * @param inIface
+	 *            the interface on which the packet was received
+	 */
+	public void handlePacket(Ethernet etherPacket, Iface inIface)
+		{
+		System.out.println("*** -> Received packet: " + etherPacket.toString().replace("\n", "\n\t"));
+
+		MACAddress src = etherPacket.getSourceMAC();
+		MACAddress dest = etherPacket.getDestinationMAC();
+
+		// add entry for src mac
+		if(macIfLookup.containsKey(src))
+			{
+			macIfLookup.remove(src);
+			}
+		macIfLookup.put(src, new SwitchTableEntry(inIface));
+		
+		// lookup entry for dest mac - check invalid timestamp
+		if(macIfLookup.containsKey(dest))
+			{
+			SwitchTableEntry outEntry = macIfLookup.get(dest);
+			// handle time 
+			LocalTime currentTime = LocalTime.now();
+			if((currentTime.compareTo(outEntry.inputTime.plusSeconds(15)) > 0))
+				{
+				// invalid entry
+				macIfLookup.remove(dest);
+				}
+			}
+		
+		// entry + valid ts
+		if(macIfLookup.containsKey(dest))
+				{
+				SwitchTableEntry outEntry = macIfLookup.get(dest);
+				this.sendPacket(etherPacket, outEntry.outIface);
+				}
+		// no entry
+		else
+			{
+			// broadcast?
+			for(Iface iface : this.interfaces.values())
+				{
+				this.sendPacket(etherPacket, iface);
+				}
+			}
+		
+		
+		/********************************************************************/
+		/* TODO: Handle packets */
+
+		/********************************************************************/
+		}
+	}
